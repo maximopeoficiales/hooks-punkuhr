aa5ae50f5b92eb784b143c35c709c6ab0c471870
Maximo Junior Apaza Chirhuana <maximopeoficiales@gmail.com>
2020-12-01 16:54:45 -0500

    se modifico hooks

--- inc/hooks.php
+++ inc/hooks.php
@@ -1,18 +1,39 @@
 <?php
 date_default_timezone_set('America/Lima');
-// function wmHome()
-// {
-//      return ["msg" => "fasdfasdf"];
-// }
 
-// add_action("rest_api_init", function () {
-//      register_rest_route("webhooks-maxco/v1", "/hola", array(
-//           "methods" => "GET",
-//           "callback" => "wmHome",
-//           'args'            => array(),
-//      ));
-// });
+/* hook de registro de cliente */
+add_action('user_register', 'hpUser_register', 10, 3);
+function hpUser_register($user_id)
+{
+     global $wpdb;
+     $fecha_actual = date("Y-m-d H:i:s");
+     $sql = "INSERT INTO wp_userssap (user_id,cod,date_created) VALUES ($user_id,0,%s)";
+     $wpdb->query($wpdb->prepare($sql, $fecha_actual));
+     $wpdb->flush();
+};
 
+// hook de actualizacion de clientes
+add_action('profile_update', 'hpUser_update', 10, 3);
+function hpUser_update($user_id, $old_user_data)
+{
+     global $wpdb;
+     $sql = "UPDATE wp_userssap SET cod = 1 WHERE user_id =$user_id";
+     $wpdb->query($sql);
+     $wpdb->flush();
+}
+
+//opcionalmente si se elimina usuario
+add_action('delete_user', 'hpDelete_user');
+function hpDelete_user($user_id)
+{
+     global $wpdb;
+     $sql = "DELETE FROM wp_userssap WHERE user_id = $user_id";
+     $wpdb->query($sql);
+     $wpdb->flush();
+     $sql = "DELETE FROM wp_prflxtrflds_user_field_data WHERE user_id = $user_id";
+     $wpdb->query($sql);
+     $wpdb->flush();
+}
 
 add_action('woocommerce_new_order', 'hpWooNewOrder');
 function hpWooNewOrder($id_order)
@@ -21,8 +42,8 @@ function hpWooNewOrder($id_order)
      $fecha_actual = date("Y-m-d H:i:s");
      $order =  wc_get_order($id_order);
      // si es una cotizacion
-     if ($order->status ==  "ywraq-new") {
-          $sql = "INSERT INTO wp_cotizaciones (id_order,customer_id,cod,date_created) VALUES ($order->id,$order->customer_id,0,%s)";
-          $wpdb->query($wpdb->prepare($sql, $fecha_actual));
-     }
+     // if ($order->status ==  "ywraq-new") {
+     $sql = "INSERT INTO wp_cotizaciones (id_order,customer_id,cod,date_created) VALUES ($order->id,$order->customer_id,0,%s)";
+     $wpdb->query($wpdb->prepare($sql, $fecha_actual));
+     // }
 }
